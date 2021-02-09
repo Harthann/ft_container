de1a27a4b5260f3a8c47b3698c561286f691d2c2
Harthann <nca.eyraud@gmail.com>
2021-02-09 21:10:14 +0100

    adding sandbox tests

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -4,55 +4,38 @@
 #include <vector>
 #include <list>
 
-namespace ft {
-
-template <bool B, class T = void>
-struct enable_if {};
-
-template <class T>
-struct enable_if<true, T> { typedef T type; } ;
-
-template <typename T, typename U>
-struct is_same { static const bool value = false ; };
-
-template <typename T>
-struct is_same<T,T> { static const bool value = true; } ;
+#include "Vector.hpp"
+#include "base_iterator.hpp"
+#include "Vector_iterator.hpp"
 
 template <class T>
-struct my_input_it {
-	typedef std::input_iterator_tag iterator_category;
-};
-
-template <class T>
-struct my_it {
-	typedef std::output_iterator_tag iterator_category;
-};
-
-template <class InputIT>
-void	is_input_it(InputIT its, InputIT ite)
+class false_it
 {
-//	typename enable_if<is_same<typename InputIT::iterator_category, std::input_iterator_tag>::value, InputIT>::type it;
-	std::cout << "Is input it\n";
-}
+	public:
 
-void	is_input_it(int a, int b)
-{
-	(void)a;
-	(void)b;
-	std::cout << "Isn't input it\n";
-}
+		typedef T	value_type;
+		typedef value_type*	pointer;
+		typedef value_type&	reference;
+		typedef std::ptrdiff_t		difference_type;
+		typedef std::input_iterator_tag iterator_category;
 
-}
+		T*	ptr;
+
+		bool operator!=(false_it &itl) { return (itl.ptr != this->ptr); };
+
+		false_it &operator++() { return (*this);}
+		false_it &operator++(int) { return (*this);}
+		false_it &operator--() { return (*this);}
+		false_it &operator--(int) { return (*this);}
+
+		reference operator*() const { return (*ptr); };
+};
 
 int main(void)
-{
-	std::vector<int> vec;
-	std::vector<int>::iterator input_its;
-	std::vector<int>::iterator input_ite;
-//	is_input_it(input_its, input_ite);
-//	is_input_it(5, 9);
-	if (ft::is_same<typename std::iterator_traits<std::vector<int>::iterator>::iterator_category, std::random_access_iterator_tag>::value)
-		std::cout << "is input iterator\n";
+ftft::vector<int> vec;
+	false_it<int> false_ite;
+	false_it<int> false_its;
+	vec.insert(vec.begin(), false_its, false_ite);
 
 	//is_input_it(input_it);
 	return (0);
