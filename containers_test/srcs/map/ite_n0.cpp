9f9782d54fc9e5f2c1da0201aa8b2beca7eb4371
Nicolas EYRAUD <nieyraud@e2r4p17.42.fr>
2021-09-06 16:02:54 +0200

    adding some little changes

--- containers_test/srcs/map/ite_n0.cpp
+++ containers_test/srcs/map/ite_n0.cpp
@@ -8,7 +8,7 @@ int		main(void)
 	TESTED_NAMESPACE::map<T1, T2> mp;
 	mp[1] = 2;
 
-	TESTED_NAMESPACE::map<T1, T2>::const_iterator ite = mp.begin();
-	*ite = 42; // < -- error
+	// TESTED_NAMESPACE::map<T1, T2>::const_iterator ite = mp.begin();
+	// *ite = 42; // < -- error
 	return (0);
 }
